a2e89c0bbb19cf2103d782ecf3aadbb727163ad4
Larry Arrington <larryarrington@pursuit.org>
2023-04-20 15:00:42 -0400

    whootwhoot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [accumulator, setAccumulator] = useState(1);
+
+  function addAccumulator() {
+    if (counter < 10) {
+      alert(`You can't afford that!`);
+      return;
+    }
+    setAccumulator(accumulator + 1);
+    setCounter(counter - 10);
+  }
+
+  const countClick = () => setCounter(counter + accumulator);
+
+  function resetGame() {
+    setCounter(0);
+    setAccumulator(1);
+  }
+
+  let gameInPlay = (
+    <>
+      <h1>Current Score: {counter} </h1>
+      <button onClick={countClick}>+{accumulator}</button>
+      <button onClick={addAccumulator}>
+        Pay 10 points to change from+{accumulator} to +{accumulator + 1}
+      </button>
+    </>
+  );
+
+  let gameWon = (
+    <div>
+      <h2>You Win!</h2>
+      <button onClick={resetGame}>Play Again?</button>
+    </div>
+  );
+
+  return <main>{counter < 100 ? gameInPlay : gameWon}</main>;
 }
 
 export default App;
