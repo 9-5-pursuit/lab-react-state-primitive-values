317b85801829ec96acad65391f91c27dc3595e6c
Jibon P <jibonp@Jibons-MacBook-Air.local>
2023-04-21 11:00:28 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-function App () {
+function App() {
+  const [score, setScore] = useState(0);
+  const [incrementor, setIncrementor] = useState(1);
+
+  const handleIncrement = () => {
+    setScore((prevScore) => prevScore + incrementor);
+  };
+
+  const handlePay = () => {
+    if (score >= 10) {
+      setScore((prevScore) => prevScore - 10);
+      setIncrementor((prevIncrementor) => prevIncrementor + 1);
+    } else {
+      alert("You can't afford that!");
+    }
+  };
+
+  const handleReset = () => {
+    setScore(0);
+    setIncrementor(1);
+  };
+
+  if (score >= 100) {
     return (
       <main>
-        <p>React State Lab</p>
+        <p className="score-text">Current Score: {score}</p>
+        <h2>You Win!</h2>
+        <button onClick={handleReset}>Play again?</button>
       </main>
     );
+  }
+
+  return (
+    <main>
+      <h2>React State Lab</h2>
+      <p className="score-text">Current Score: {score}</p>
+      <div className="increment-container">
+        <button className="increment-button" onClick={handleIncrement}>
+          +{incrementor}
+        </button>
+        <button className="pay-button" onClick={handlePay}>
+          Pay 10 points to change from +{incrementor} to +{incrementor + 1}
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default App;
