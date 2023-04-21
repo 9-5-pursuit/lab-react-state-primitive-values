aa9c615f6d5ffc3c056995467bc844da84174b4e
Chantal <chantalgomez@pursuit.org>
2023-04-20 21:41:45 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
+
+  const [counter, setCounter] = useState(0)
+  const [increase, setIncrease] = useState(1)
+
+  function handleCounterAddOne(){
+    setCounter(counter + 1)
+
+  }
+  function handleIncrease(){
+    if(counter <= 10){
+
+    }
+    setIncrease(increase + 1)
+
+  }
     return (
       <main>
         <p>React State Lab</p>
+        <h1>Counter Score: {counter}</h1>
+        <button onClick={handleCounterAddOne}>+{increase}</button>
+        <button onClick={handleIncrease}>Pay 10 points to change from +{} to +{}</button>
+        
       </main>
     );
 }
