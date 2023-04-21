cc1117879348a34ea8f914d3cf7064774bd3d7e4
Nicole Rodriguez <nicolerodriguez@nicoles-air.mynetworksettings.com>
2023-04-20 23:49:27 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [points, setPoints] = useState(1);
+
+  function incrementScore() {
+    setCounter(counter + points);
+  }
+
+  function payPoints() {
+    if (counter >= 10) {
+      setPoints(points + 1);
+      setCounter(counter - 10);
+    } else {
+      alert("You cannot afford that!");
+    }
+  }
+
+  function resetGame() {
+    setCounter(0);
+    setPoints(1);
+  }
+
+  return (
+    <main>
+      {counter < 100 ? (
+        <>
+          <p> Current Score: {counter}</p>
+          <button onClick={incrementScore}>+{points}</button>
+          <button onClick={payPoints}>
+            Pay 10 points to change from {points} to {points + 1}
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>You Win!</h2>
+          <button onClick={resetGame}> Play again?</button>
+        </>
+      )}
+    </main>
+  );
 }
 
 export default App;
