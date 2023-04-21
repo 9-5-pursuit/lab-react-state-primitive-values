71c01aa0781014746cfff76c2d6f8c5f49dd6ed5
Michael Reyes <michaelreyes@Michaels-Air.fios-router.home>
2023-04-21 10:11:16 -0400

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [score, setScore] = useState(0);
+  const [incrementor, setIncrementor] = useState(1);
+
+  const click = () => {
+    setScore(score + incrementor);
+  };
+
+  const upgrade = () => {
+    if (score >= 10) {
+      setScore(score - 10);
+      setIncrementor(incrementor + 1);
+    } else {
+      alert("You can't afford that!");
+    }
+  };
+
+  const handleRestartClick = () => {
+    setScore(0);
+    setIncrementor(1);
+  };
+
+  return (
+    <div>
+      <h1>Clicker Game</h1>
+      <h2>Current Score: {score}</h2>
+      {score < 100 && (
+        <div>
+          <button onClick={click}>+{incrementor}</button>
+          <br />
+          <button onClick={upgrade}>
+            Pay 10 points to change from +{incrementor} to +{incrementor + 1}
+          </button>
+        </div>
+      )}
+      {score >= 100 && (
+        <div>
+          <h2>You Win!</h2>
+          <button onClick={handleRestartClick}>Play again?</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
