faf8ff65e11dfd67e680d42a9838e7caa6f18c8f
Mussie <mussierussom@pursuit.org>
2023-04-21 09:51:03 -0400

    React-state-prim-val lab is done for now.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [increase, setIncrease] = useState(1);
+  //const [counterTracker, setCounterTracker] = useState(counter);
+
+  function increaseBy() {
+    setCounter(counter + increase);
+  }
+
+  function increaseByTen() {
+    if (counter >= 10) {
+      setCounter(counter - 10);
+      setIncrease(increase + 1);
+    } else {
+      alert("You can't afford that!");
+    }
+  }
+  function overHundred() {
+    if (counter >= 100) {
+      <div>
+        <h2>You Win!</h2>
+      </div>;
+    }
+  }
+  // function resetButton() {
+  //   setCounter(counter - counter);
+  // }
+
+  return (
+    <main>
+      <div>
+        <button onClick={() => increaseBy(increase)}>+{increase}</button>
+        <button onClick={() => increaseByTen()}>
+          Pay 10 points to change from +{increase} to +{increase + 1}
+        </button>
+        <p>Current Score: {counter}</p>
+        <div>{overHundred()}</div>
+      </div>
+      <div>
+        <button onClick="">Play again?</button>
+      </div>
+    </main>
+  );
 }
 
 export default App;
