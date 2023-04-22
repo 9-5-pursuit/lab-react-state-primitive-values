62d4863f2a262ee9bf39434d7931945fb64135fd
Erika Medina <erikamedina@pursuit.org>
2023-04-22 06:56:34 -0400

    button

---
+++ src/IncrementButtons.js
@@ -0,0 +1,15 @@
+// // IncrementButtons.js
+// import React from "react";
+
+// function IncrementButtons({ incrementValue, handleScoreClick, handleIncrementValueClick }) {
+//   return (
+//     <>
+//       <button onClick={handleScoreClick}>+{incrementValue}</button>
+//       <button onClick={handleIncrementValueClick}>
+//         Pay 10 points to change from +{incrementValue} to +{incrementValue + 1}
+//       </button>
+//     </>
+//   );
+// }
+
+// export default IncrementButtons;
---
+++ src/scoreDisplay.js
@@ -0,0 +1,8 @@
+// // ScoreDisplay.js
+// import React from "react";
+
+// function ScoreDisplay({ currentScore }) {
+//   return <h1>Current Score: {currentScore}</h1>;
+// }
+
+// export default ScoreDisplay;
