c730e7eb0924d3cf1e52f232c870e154d0b41a10
rboyce212 <richboyce@pursuit.org>
2023-04-22 07:14:11 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -4,41 +4,50 @@ import "./App.css";
 function App() {
   const [score, setScore] = useState(0);
   const [points, setPoints] = useState(1);
+  const pointsToWin = 100;
 
   function levelUp() {
-    if (score < 10) {
+    if (score >= 10) {
+      setPoints(points + 1);
+      setScore(score - 10);
+    } else {
       alert("You can't afford that!");
-      return;
     }
-    setScore(score - 10);
-    setPoints(points + 1);
   }
-
-  const clickCounter = () => setScore(score + points);
 
   function reset() {
     setScore(0);
     setPoints(1);
   }
 
-  let gamePlay = (
-    <>
-      <h1>Current Score: {score}</h1>
-      <button onClick={clickCounter}>+{points}</button>
-      <button onClick={levelUp}>
-        Pay 10 points to change from +{points} to +{points + 1}
-      </button>
-    </>
-  );
+  function clickCounter() {
+    setScore(score + points);
+  }
 
-  let winGame = (
-    <>
-      <h2>You Win!</h2>
-      <button onClick={reset}>Play Again?</button>
-    </>
+  return (
+    <main>
+      {score < pointsToWin ? (
+        <>
+          <p>Current Score: {score}</p>
+          <button onClick={clickCounter}>+{points}</button>
+          <button onClick={levelUp}>
+            Pay 10 points to change from +{points} to +{points + 1}
+          </button>
+        </>
+      ) : (
+        <div>
+          <img
+            src="src/images/youwin.gif"
+            alt="winner"
+            width="500"
+            height="500"
+          />
+          <h2>You Win!</h2>
+          <button onClick={reset}>Play again?</button>
+        </div>
+      )}
+    </main>
   );
-
-  return <main>{score < 100 ? gamePlay : winGame}</main>;
 }
 
 export default App;
