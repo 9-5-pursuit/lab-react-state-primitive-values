5e369449d8859bd6b3df9daab5f3350658299149
Rubby <rubenslombard@pursuit.org>
2023-04-24 00:10:17 -0400

    modified files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
-function App () {
+
+function App() {
+  
+  const [counting, setCounting] = useState(0);
+  
+
+   function payingPoints() {
+     if (counting < 10) {
+       alert("You can't afford that!");
+     } else if (counting > 10) {
+       setCounting(counting - 10);
+     }
+   }
+  
+    function addScore() {
+      setCounting(counting + 1);
+    }
+ 
     return (
       <main>
-        <p>React State Lab</p>
+        <p>Current Score: {counting}</p>
+
+        <div>
+          <button className="play" onClick={() => addScore()}>
+            Click me to Play
+          </button>
+        </div>
+
+        <div>
+          <button className="payPoints" onClick={() => payingPoints()}>
+            Pay 10 points to change from +1 to +2
+          </button>
+        </div>
+
+
       </main>
     );
 }
 
-export default App;
+  export default App;
