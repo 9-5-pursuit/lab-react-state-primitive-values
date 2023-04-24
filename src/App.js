a443a942700f0ef8660bcde6fd3155132a12ce71
daleskiii <aun2793@gmail.com>
2023-04-24 09:51:53 -0400

    state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-function App () {
-    return (
+function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+
+  function changeScore() {
+    if (currentScore >= 10) {
+      setCurrentScore(currentScore + 2);
+    } else if (currentScore >= 25) {
+      setCurrentScore(currentScore + 5);
+    } else {
+      setCurrentScore(currentScore + 1);
+    }
+  }
+  return (
+    <>
       <main>
-        <p>React State Lab</p>
+        <p>Current Score:{currentScore} </p>
+        <button onClick={() => changeScore()}>
+          {currentScore >= 25 ? "+5" : currentScore >= 10 ? "+2" : "+1"}
+        </button>
       </main>
-    );
+    </>
+  );
 }
 
 export default App;
