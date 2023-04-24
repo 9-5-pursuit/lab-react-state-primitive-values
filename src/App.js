23338f1ce2af9b4b0409b8e9eddd924921f75f68
charellpeterkin <your_email_address@example.com>
2023-04-24 11:14:24 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -4,25 +4,40 @@ import "./App.css";
 
 function App () {
   const [count, setCount] = useState(0)
-  const [ten, setTen] = useState(true)
+  const [increase, setIncrease] = useState(1)
+
 
   function countClicker() {
-    setCount(count + 1)
+    setCount(count + increase)
   }
 
   function spendTen() {
 
     if (count < 10) {
-      alert("You can't afford that!")
+      return alert("You can't afford that!")
+    } else {
+      setCount(count - 10)
+      setIncrease(increase + 1)
     }
   }
 
     return (
+      <main>
+        {count < 100 ? (
+        <>
       <div className="App" style={{textAlign: "center"}}>
         <p>Current Score: {count}</p>
-         <button onClick={countClicker}>+{count}</button>
-         <button onClick={spendTen}>Pay 10 points to change from +1 to +2</button>
+         <button onClick={countClicker}>+{increase}</button>
+         <button onClick={spendTen}>Pay 10 points to change from {increase} to {increase + 1}</button>
       </div>
+      </>
+        ) : (
+      <>
+      <h2>You win!</h2>
+      <button onClick={{}=false}>Play Again!</button>
+      </>
+      )} 
+      </main>
     );
 }
 
