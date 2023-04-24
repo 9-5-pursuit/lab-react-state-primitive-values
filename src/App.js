e352b77bbcf1b571f41f5534e5dc55e63dfa8e19
charellpeterkin <your_email_address@example.com>
2023-04-24 09:25:09 -0400

    primitive lab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import button from "./button";
 import "./App.css";
 
 function App () {
+  const [count, setCount] = useState(0)
+  const [ten, setTen] = useState(true)
+
+  function countClicker() {
+    setCount(count + 1)
+  }
+
+  function spendTen() {
+
+    if (count < 10) {
+      alert("You can't afford that!")
+    }
+  }
+
     return (
-      <main>
-        <p>React State Lab</p>
-      </main>
+      <div className="App" style={{textAlign: "center"}}>
+        <p>Current Score: {count}</p>
+         <button onClick={countClicker}>+{count}</button>
+         <button onClick={spendTen}>Pay 10 points to change from +1 to +2</button>
+      </div>
     );
 }
 
