b76011341683c186bd3682e2782bcc37f0a542c5
Devin Lewis <devinlewis@pursuit.org>
2023-04-25 15:46:49 -0400

    Pass all test & layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App () {
+function App() {
+    const [totalPoints, setPoints] = useState(1);
+    const [currentScore, setScore] = useState(0);
+
+    function updateScore() {
+        setScore(currentScore + totalPoints);
+    }
+
+    function updatePoints() {
+        if (currentScore >= 10) {
+            setScore(currentScore - 10);
+            setPoints(totalPoints + 1);
+        } else {
+            window.alert("You can't afford that!");
+        }
+    }
+
+    function resetScore() {
+        setScore(0);
+        setPoints(1);
+    }
+
     return (
-      <main>
-        <p>React State Lab</p>
-      </main>
+        <main>
+            {currentScore < 100 ? (
+                <div>
+                    <h1>Current Score: {currentScore}</h1>
+                    <p>
+                        <button onClick={updateScore}>+{totalPoints}</button>
+                    </p>
+                    <p>
+                        <button onClick={updatePoints}>
+                            Pay 10 points to change from +{totalPoints} to +
+                            {totalPoints + 1}
+                        </button>
+                    </p>
+                </div>
+            ) : (
+                <div>
+                    <h2>You Win!</h2>
+                    <p>
+                        <button onClick={resetScore}>Play again?</button>
+                    </p>
+                </div>
+            )}
+        </main>
     );
 }
 
