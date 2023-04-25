0f6b25438b6e0f4c63ab8cda6c33b62562395e46
Mussie <mussierussom@pursuit.org>
2023-04-25 00:17:04 -0400

    React-state-primitive-values lab is succesfully completed.

--- src/App.js
+++ src/App.js
@@ -4,46 +4,50 @@ import "./App.css";
 function App() {
   const [counter, setCounter] = useState(0);
   const [increase, setIncrease] = useState(1);
-  //const [counterTracker, setCounterTracker] = useState(counter);
 
   function increaseBy() {
     setCounter(counter + increase);
   }
-
   function increaseByTen() {
-    if (counter >= 10) {
+    if (counter < 10) {
+      alert("You can't afford that!");
+    } else {
       setCounter(counter - 10);
       setIncrease(increase + 1);
-    } else {
-      alert("You can't afford that!");
     }
   }
-  function overHundred() {
-    if (counter >= 100) {
-      <div>
-        <h2>You Win!</h2>
-      </div>;
-    }
-  }
-  // function resetButton() {
-  //   setCounter(counter - counter);
+  // function overHundred() {
+  //   if (counter >= 100) {
+  //     return (
+  //       <div>
+  //         <h2>You Win!</h2>
+  //       </div>
+  //     );
+  //   }
   // }
+
+  function reset() {
+    setCounter(0);
+    setIncrease(1);
+  }
 
   return (
     <main>
-      <div>
-        <button onClick={() => increaseBy(increase)}>+{increase}</button>
-        <button onClick={() => increaseByTen()}>
-          Pay 10 points to change from +{increase} to +{increase + 1}
-        </button>
-        <p>Current Score: {counter}</p>
-        <div>{overHundred()}</div>
-      </div>
-      <div>
-        <button onClick="">Play again?</button>
-      </div>
+      {counter < 100 ? (
+        <>
+          <h1>Current Score: {counter}</h1>
+          <button onClick={() => increaseBy(increase)}>+{increase}</button>
+          <button onClick={() => increaseByTen()}>
+            Pay 10 points to change from +{increase} to +{increase + 1}
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>You Win!</h2>
+          <button onClick={reset}>Play again?</button>
+        </>
+      )}
     </main>
   );
 }
-
 export default App;
