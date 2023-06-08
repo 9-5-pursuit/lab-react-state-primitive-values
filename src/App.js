840ab8f4dfa5ecfde8081947a72c601a8bf294f3
Akira Watson-Brown <akirawatson-brown@Akiras-Air.home>
2023-06-08 00:10:09 -0400

    FullyFunctional-AB

--- src/App.js
+++ src/App.js
@@ -1,77 +1,111 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, setCount] = useState(0);
-  const [increase, setIncrease] = useState(1);
-  const [cost, setCost] = useState(1);
-  //const [isSeen, setVisible] = useState("hidden");
-  const [initialSeen, setInitial] = useState("");
-  const [currentHide, setHide] = useState("hidden");
-
-  function increment() {
-    checkWin();
-    setCount(counter + increase);
-  }
-  function changeCost() {
-    if (counter >= cost) {
-      setCount(counter - increase); // Subtracts the cost from the current score
-      setIncrease(increase + cost); //Changes the increment by new value
-      setCost(cost + 1); // increases the cost by 1
-    } else {
-      alert("You can't afford this");
-    }
-  }
-  function checkWin() {
-    if (counter >= 100) {
-      //end game
-      //console.log("You've won");
-      setInitial("hidden");
-      setHide("");
-      //changeVisible();
-    }
-  }
-  // function changeVisible() {
-  //   const targetLocal = document.querySelector("main");
-  //   const targetChildArr = targetLocal.childNodes;
-
-  //   targetChildArr.forEach((element) => {
-  //     const targetClass = element.className;
-  //     toggleVisible(targetClass);
-  //     console.log(element.className);
-  //   });
+  /* 
+  // const [counter, setCount] = useState(0);
+  // const [increase, setIncrease] = useState(1);
+  // const [cost, setCost] = useState(1);
+  // //const [isSeen, setVisible] = useState("hidden");
+  // const [initialSeen, setInitial] = useState("");
+  // const [currentHide, setHide] = useState("hidden");
+  // function increment() {
+  //   setCount(counter + increase);
+  //   console.log(counter);
+  //   checkWin();
   // }
-  function resetStates() {
-    setCount(0);
-    setIncrease(1);
-    setCost(1);
-    setInitial("");
-    setHide("hidden");
-  }
-
-  // function toggleVisible(Class) {
-  //   if (Class === "") {
-  //     this.setVisible("hidden");
+  // function changeCost() {
+  //   if (counter >= cost) {
+  //     setCount(counter - increase); // Subtracts the cost from the current score
+  //     setIncrease(increase + cost); //Changes the increment by new value
+  //     setCost(cost + 1); // increases the cost by 1
   //   } else {
-  //     this.setVisible("");
+  //     alert("You can't afford this");
   //   }
   // }
+  // function checkWin() {
+  //   if (counter >= 100) {
+  //     //end game
+  //     //console.log("You've won");
+  //     setInitial("hidden");
+  //     setHide("");
+  //     //changeVisible();
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  // function resetStates() {
+  //   setCount(0);
+  //   setIncrease(1);
+  //   setCost(1);
+  //   setInitial("");
+  //   setHide("hidden");
+  // }
+  // //----------------------------------------------------------------//
+  // return (
+  //   <main>
+  //     <h1>Current Score: {counter} </h1>
+  //    
+  //     <button onClick={increment} className={initialSeen}>
+  //       +{increase}
+  //     </button>
+  //     <button onClick={changeCost} className={initialSeen}>
+  //       Pay {cost} points to change from +{increase} to +{increase + 1}
+  //     </button>
+  //     <h2 className={currentHide}>YOU WIN !</h2>
+  //     <button onClick={resetStates} className={currentHide}>
+  //       Play Again
+  //     </button>
+  //   </main>
+  // );
+  */
+
+  const [counter, setCounter] = useState(0);
+  const [hasWon, setHasWon] = useState(false);
+  const [increment, setIncrement] = useState(1);
+  const cost = 10;
+  ///-------------------------------------------------------------------//
+
+  function increaseCounter() {
+    setCounter(counter + increment);
+  }
+  useEffect(() => {
+    //console.log(counter);
+    if (counter >= 100) {
+      setHasWon(true);
+    }
+  }, [counter]);
+  function changeIncrement() {
+    if (counter >= cost) {
+      setIncrement(increment + 1);
+      setCounter(counter - cost);
+    } else {
+      alert("You can't afford that!");
+    }
+  }
+
+  function resetStates() {
+    setCounter(0);
+    setHasWon(false);
+    setIncrement(1);
+  }
   return (
     <main>
-      <h1>Current Score: {counter} </h1>
-
-      {/* <p>React State Lab</p> */}
-      <button onClick={increment} className={initialSeen}>
-        +{increase}
-      </button>
-      <button onClick={changeCost} className={initialSeen}>
-        Pay {cost} points to change from +{increase} to +{increase + 1}
-      </button>
-
-      <h2 className={currentHide}>YOU WIN !</h2>
-      <button onClick={resetStates} className={currentHide}>
-        Play Again
-      </button>
+      {!hasWon ? (
+        <>
+          <h1>Current Score: {counter} </h1>
+          <button onClick={increaseCounter}>+{increment}</button>
+          <button onClick={changeIncrement}>
+            Pay {cost} points to change from +{increment} to +{increment + 1}
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>You Win!</h2>
+          <button onClick={resetStates}>Play again?</button>
+        </>
+      )}
     </main>
   );
 }
